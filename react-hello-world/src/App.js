74bd6e99b8aadc801f01278fc03ac18777e6dbc7
veja947 <veja947@gmail.com>
2021-01-27 23:00:39 -0500

    bring state in component

--- react-hello-world/src/App.js
+++ react-hello-world/src/App.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-            <Person name="Name A" age="18"/>
-            <Person name="Name B" age="29"/>
-            <Person name="Name C" age="35">Note: This is note for Name C</Person>
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        persons: [
+            {
+                name: 'name 1',
+                age: 18
+            },
+            {
+                name: 'name 2',
+                age: 28
+            },
+            {
+                name: 'name 3',
+                age: 38
+            }
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                        Note: this is person 2
+                    </Person>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
